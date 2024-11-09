fe874d9a67cabe590510faedca780d56b2ca3806
Rekha N Samudra <rekhansamudra@Rekha-N-Samudra.local>
2024-11-09 06:35:00 +0530

    home page done

---
+++ frontend/src/Components/Header.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { assets } from "../assets/assets_frontend/assets";
+
+const Header = () => {
+  return (
+    <>
+      {/*  ----- left side ----- */}
+      <div className="flex flex-col md:flex-row flex-wrap bg-green-500 rounded-lg px-6 md:px-10 lg:px-20 my-5 mx-32">
+        <div className="md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]">
+          <p className="text-white text-3xl md:text-4xl lg:text-5xl font-semibold leading-tight md:leading-tight lg:leading-tight">
+            Book Appointment <br />
+            With <span className="text-primary">Trusted</span> Doctors
+          </p>
+          <div className="text-white flex flex-col md:flex-row items-center gap-3 text-sm font-light">
+            <img className="w-28" src={assets.group_profiles} alt="" />
+            <p>
+              Simply browse through our extensive list of trusted doctors,{" "}
+              <br className="hidden sm:block" />
+              schedule your appointment hassle-free.
+            </p>
+          </div>
+          <a
+            href="#speciality"
+            className="flex gap-2 justify-center bg-white text-gray-600 rounded-full px-8 py-3 w-60 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300"
+          >
+            <span className="text-primary font-medium">Book appointment </span>
+            <img className="w-4" src={assets.arrow_icon} alt="" />
+          </a>
+        </div>
+        {/* ----- right side ----- */}
+        <div className="md:w-1/2 relative">
+          <img
+            className="w-full md:absolute bottom-0 h-auto rounded-lg"
+            src={assets.header_img}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
--- frontend/src/Components/NavBar.jsx
+++ frontend/src/Components/NavBar.jsx
@@ -1,13 +1,19 @@
-import React from "react";
-import logo from "../assets/logo.png";
+import React, { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
+import { assets } from "../assets/assets_frontend/assets";
 
 const NavBar = () => {
+  const [showMenu, setShowMenu] = useState(true);
+  const [token, setToken] = useState(true);
   const navigate = useNavigate();
   return (
     <>
-      <div className="flex items-center justify-between text-sm border-b border-b-gray-400 pt-4 mb-1 mx-16">
-        <img src={logo} alt="sickfix logo" className="w-44 cursor-pointer" />
+      <div className="flex items-center justify-between text-sm border-b border-b-gray-400 pt-4 mb-1 mx-32">
+        <img
+          src={assets.logo}
+          alt="sickfix logo"
+          className="w-44 cursor-pointer"
+        />
         <ul className="hidden md:flex items-center gap-5 font-medium">
           <NavLink to={"/"}>
             <li>Home</li>
@@ -26,12 +32,51 @@ const NavBar = () => {
             <hr className="border-none outline-none h-0.5 bg-primary w-3/4 m-auto hidden mt-1" />
           </NavLink>
         </ul>
-        <button
-          onClick={() => navigate("/login")}
-          className="bg-primary text-white rounded-full p-3 font-medium hidden md:block"
-        >
-          Create Account
-        </button>
+        <div className="flex items-center gap-4">
+          {token ? (
+            <div className="flex items-center gap-2 cursor-pointer group relative">
+              <img
+                className="rounded-full w-12"
+                src={assets.profile_pic}
+                alt="my profile"
+              />
+              <img
+                className="w-3"
+                src={assets.dropdown_icon}
+                alt="dropdown icon"
+              />
+              <div className="absolute top-6 right-0 pt-4 text-base font-medium text-gray-600 z-20 hidden group-hover:block mt-5">
+                <div className="min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4">
+                  <p
+                    onClick={() => navigate("my-profile")}
+                    className="hover:text-black font-medium"
+                  >
+                    My Profile
+                  </p>
+                  <p
+                    onClick={() => navigate("/my-appointments")}
+                    className="hover:text-black font-medium"
+                  >
+                    My Appointments
+                  </p>
+                  <p
+                    onClick={() => setToken(false)}
+                    className="hover:text-black font-medium"
+                  >
+                    Logout
+                  </p>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <button
+              onClick={() => navigate("/login")}
+              className="bg-primary text-white rounded-full p-3 font-medium hidden md:block"
+            >
+              Create Account
+            </button>
+          )}
+        </div>
       </div>
     </>
   );
