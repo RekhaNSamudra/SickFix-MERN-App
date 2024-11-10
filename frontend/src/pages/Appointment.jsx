2299cf2b4fd8970c20c8697a9d32df33c0676ee4
Rekha N Samudra <rekhansamudra@Rekha-N-Samudra.local>
2024-11-10 19:06:31 +0530

    contact and about pages added

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
+import { AppContext } from "../Context/AppContext";
+import { assets } from "../assets/assets_frontend/assets";
 
 const Appointment = () => {
-  return <div>Appointment</div>;
+  const { docId } = useParams();
+  const { doctors, currencySymbol } = useContext(AppContext);
+
+  // Find the doctor with the matching ID
+  const doctor = doctors.find((doc) => doc._id === docId);
+
+  return (
+    doctor && (
+      <div className=" mx-32">
+        <div className="flex flex-col sm:flex-row gap-4 mt-6">
+          <div>
+            <img
+              className="bg-primary w-full sm:max-w-72 rounded-lg"
+              src={doctor.image}
+              alt=""
+            />
+          </div>
+
+          <div className="flex-1 border border-gray-400 rounded-lg p-8 py-7 mx-2 sm:mx-0 sm:mt-0 mt-[-80px]">
+            <p className="flex items-center gap-2 text-gray-900 text-2xl font-medium">
+              {doctor.name}
+              <img className="w-5" src={assets.verified_icon} alt="" />
+            </p>
+            <div className="flex items-center gap-2 text-gray-600 text-sm mt-1">
+              <p>
+                {doctor.degree} - {doctor.speciality}
+              </p>
+              <button className="border rounded-full py-0.5 px-2 text-xs ">
+                {doctor.experience}
+              </button>
+            </div>
+            <div>
+              <p className="text-gray-900 text-lg mt-3 font-medium">About</p>
+              <p className="text-gray-600 text-sm max-w-[700px] mt-1">
+                {doctor.about}
+              </p>
+            </div>
+            <p className="text-gray-500 mt-4 font-medium">
+              Appointment fee:
+              <span className="text-gray-700">
+                {currencySymbol}
+                {doctor.fees}
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    )
+  );
 };
 
 export default Appointment;
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../assets/assets_frontend/assets";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <>
+      <div className="mx-32">
+        <div className="text-center text-2xl text-gray-500 pt-10">
+          <p>
+            CONTACT <span className="text-gray-700 font-semibold ">US</span>
+          </p>
+        </div>
+        <div className="flex flex-col justify-center my-10 md:flex-row gap-10 mb-28 text-sm">
+          <img
+            className="w-full max-w-[360px]"
+            src={assets.contact_image}
+            alt=""
+          />
+          <div className="flex flex-col justify-center items-start  gap-6">
+            <p className="text-gray-600 text-lg font-semibold">Our OFFICE</p>
+            <p className="text-gray-500">
+              54709 Willms Station <br /> Suite 350, Washington, USA
+            </p>
+            <p className="text-gray-500">
+              Tel: (415) 555‑0132 <br /> Email: greatstackdev@gmail.com{" "}
+            </p>
+            <p className="text-gray-600 text-lg font-semibold">
+              Careers at SICKFIX{" "}
+            </p>
+            <p className="text-gray-500">
+              {" "}
+              Learn more about our teams and job openings.
+            </p>
+            <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">
+              {" "}
+              Explore Jobs
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Contact
+export default Contact;
